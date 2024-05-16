dad66961cae5f124a43d92e70f58d1e1f3563669
Auzfest <114009523+Auzfest@users.noreply.github.com>
2024-05-16 09:32:31 -0600

    Team 3 activity complete

---
+++ src/js/components/ProductList.svelte
@@ -0,0 +1,18 @@
+<script>
+    import ProductSummary from "./ProductSummary.svelte";
+    import { getData } from "../productData.mjs";
+    
+    export let category;
+    let promise = getData(category);
+</script>
+    
+    <p>Top products: {category}</p>
+    {#await promise}
+        <p>Loading...</p>
+    {:then products}
+        <ul class="product-list">
+    {#each products as product}
+        <li class="product-card"><ProductSummary {product}/></li>
+    {/each}
+        </ul>
+    {/await}
